2f96ba9cbf9d7774889feb5a3eb89386f6767c2b
TAUHIDAALAM <tauhidaalam2705@gmail.com>
2021-09-18 00:39:10 +0530

    new git testing

--- src/test/java/org/testng_sample/Testngpractice.java
+++ src/test/java/org/testng_sample/Testngpractice.java
@@ -9,19 +9,9 @@ import org.testng.annotations.Test;
 
 public class Testngpractice extends BaseClass {
 
-	
-	@Parameters({ "Username", "password" })
-	@Test
 
-	private void homepage(String names, String passed) {
-
-		chromeBrowser();
-		openUrl("https://www.facebook.com/");
-		WebElement number = driver.findElement(By.name("email"));
-		number.sendKeys(names);
-		WebElement pwd = driver.findElement(By.name("pass"));
-		pwd.sendKeys(passed);
-
+	public static void main(String[] args) {
+		System.out.println("testring Git");
 	}
 
 }
